09c8f2377e54116bec93c0153088f373839279a7
OptionsBuffetDev <mcrafter147@gmail.com>
2025-03-16 17:34:16 +0530

    [Fix]: Fixed Unmatched route issue

--- user_app/app/(tabs)/_layout.tsx
+++ user_app/app/(tabs)/_layout.tsx
@@ -3,13 +3,12 @@ import { Tabs } from "expo-router";
 export default function TabLayout() {
   return (
     <Tabs 
-      initialRouteName="home"
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: true,
       }}
     >
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
--- user_app/app/_layout.tsx
+++ user_app/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack initialRouteName="(tabs)" screenOptions={{ headerShown: false }}>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
